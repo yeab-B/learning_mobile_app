eca48a37b34121dd606d88e700afde884f18d263
Yabe-B <yeabsirabehailu92@gmail.com>
2025-12-04 21:13:54 +0300

    Add Task model with factory and serialization methods

---
+++ taskly/lib/models/task.dart
@@ -0,0 +1,25 @@
+
+class Task{
+  String content;
+  DateTime timestamp;
+  bool done;
+  Task({
+    required this.content,
+    required this.timestamp,
+    required this.done,
+  });
+  factory Task.fromMap(Map task){
+    return Task(
+      content: task['content'],
+      timestamp: DateTime.parse(task['timestamp']),
+      done: task['done'],
+    );
+  }
+  Map toMap(){
+    return {
+      'content': content,
+      'timestamp': timestamp.toIso8601String(),
+      'done': done,
+    };
+  }
+}
