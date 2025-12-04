a941fea578bed89f95fb94ee11b56de9596efe06
Yabe-B <yeabsirabehailu92@gmail.com>
2025-12-04 13:50:16 +0300

    Add HomePage widget and update main app structure

---
+++ taskly/lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+   HomePage();
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
